575777d3beb12d597ad57ba4078dcc9abdc3de13
ProfBarcelli <88441587+ProfBarcelli@users.noreply.github.com>
2024-04-11 09:52:21 +0200

    added test floor and character
    we experimented a bit with scripting, in particular the physics of motion, and with collisions and gravity

---
+++ Party Time!/personaggio.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+const g = 9.81
+var n_salti = 0 
+var max_salti = 2
+func _physics_process(delta):
+	var d = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		d.x = -1
+	if Input.is_action_pressed("ui_right"):
+		d.x = 1
+	if Input.is_action_pressed("ui_down"):
+		var old_velocity = velocity
+		velocity = Vector2(1000,0)
+		move_and_slide()
+		velocity = old_velocity
+	if d.length()==0:
+		velocity.x *= 0.95
+	else:
+		var accel = d*200
+		velocity += accel * delta
+	if not is_on_floor():
+		velocity.y += g*delta*100
+	else:
+		n_salti = 0
+	if Input.is_action_just_pressed("ui_up") and n_salti<max_salti :
+		velocity.y += -300
+		n_salti += 1
+	move_and_slide()
