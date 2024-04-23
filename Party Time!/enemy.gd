3ab989a4e358617764c3199b11ac08b9c011edc5
GXGAGGEX <alessandro.gagge22@gmail.com>
2024-04-23 11:20:31 +0200

    updated enemy movement

--- Party Time!/enemy.gd
+++ Party Time!/enemy.gd
@@ -2,19 +2,21 @@ extends CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-const SPEED = 300.0
-const JUMP_VELOCITY = 400.0
+@export var player:CharacterBody2D = null
+
+const SPEED = 150.0
 
 func _ready():
 	get_node("/root/livello1/Enemy")
 	
 
-##func _physics_process(delta):
-	##var player = get_node("res://personaggio.tscn")
-	##var direction = global_position.direction_to(player.global_position)
-	##velocity = direction * SPEED
-	##if not is_on_floor():
-		##velocity.y += gravity * delta
+func _physics_process(delta):
+	if player==null or player.global_position.distance_to(global_position)>250:
+		return
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * SPEED
+	if not is_on_floor():
+		velocity.y += gravity * delta
 		
 
 
